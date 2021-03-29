5b66e15a69000facd0b70d905ce793852e4c9c08
Lorenzo Setale <lorenzo@setale.me>
2021-03-29 18:20:27 +0000

    adds twitch basic listener
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

--- generator/main.go
+++ generator/main.go
@@ -1,1 +1,11 @@
 package generator
+
+import (
+	"log"
+)
+
+// GenerateMeme does what it says
+func GenerateMeme(kind, text string) {
+	// TODO: DO IT
+	log.Printf(`[%s] %s`, kind, text)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module gitlab.com/koalalorenzo/twitch-meme-generator
 
 go 1.16
+
+require (
+	github.com/gempir/go-twitch-irc/v2 v2.5.0
+	github.com/spf13/cobra v1.1.3
+)
