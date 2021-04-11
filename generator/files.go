bfb03a5a7637bdae117d378d3283207143a597b4
Lorenzo Setale <lorenzo@setale.me>
2021-04-11 11:19:11 +0200

    restructure meme loader to avoid files not found
    Signed-off-by: Lorenzo Setale <lorenzo@setale.me>

--- generator/files.go
+++ generator/files.go
@@ -3,35 +3,88 @@ package generator
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"image/gif"
 	"image/jpeg"
 	"image/png"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/jpoz/gomeme"
+	log "github.com/sirupsen/logrus"
 )
+
+// MemeFiles these are the file names of the memes available from the bot
+var MemeFiles []*MemeFile
+
+type MemeFile struct {
+	Kind      string
+	Filename  string
+	FontSize  float64
+	Extension string
+}
 
 var (
 	// This is the path where we store all the images.
 	assetsDirPath string
 )
 
-func getMemesFilesData() (l []string, err error) {
-	l = []string{}
+func getMemesFilesData() (l []*MemeFile, err error) {
+	logWF := log.WithFields(log.Fields{
+		"f": "generator.getMemesFilesData",
+	})
+
+	l = []*MemeFile{}
 	files, err := os.ReadDir(assetsDirPath)
 	if err != nil {
-		return []string{}, err
+		return []*MemeFile{}, err
 	}
 
 	for _, file := range files {
 		if file.IsDir() {
 			continue
 		}
+		mfn := file.Name()
+		logWF = logWF.WithField("file", mfn)
 
-		l = append(l, file.Name())
+		mfsp := strings.Split(mfn, ".")
+
+		if len(mfsp) < 3 {
+			logWF.Debugf("Error, file has wrong fomrat. Expecting <kind>.<font-size>.<extension>")
+			continue
+		}
+
+		fontSize, err := strconv.ParseFloat(mfsp[1], 64)
+		if err != nil {
+			logWF.Warnf("Error defining the fontSize: %s", err.Error())
+			continue
+		}
+
+		m := &MemeFile{
+			Filename:  fmt.Sprintf("assets/%s", mfn),
+			Kind:      mfsp[0],
+			FontSize:  fontSize,
+			Extension: mfsp[2],
+		}
+		l = append(l, m)
 	}
 	return l, nil
+}
+
+func tickerFilesLiveLoade() {
+	logWF := log.WithFields(log.Fields{
+		"f":      "generator.tickerFilesLiveLoade",
+		"assets": assetsDirPath,
+	})
+
+	mfs, err := getMemesFilesData()
+	if err != nil {
+		logWF.Errorf("Erorr loading files: %s", err.Error())
+	}
+
+	MemeFiles = mfs
 }
 
 func detectFileType(in []byte) (n gomeme.Memeable, err error) {
